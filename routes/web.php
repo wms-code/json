c8c305ddf60adeb358226cee90d528ba79906f9d
satheesh <satheesh@dreamstudio.in>
2020-10-28 21:06:43 +0530

    app

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,33 @@
 <?php
 
+use App\Models\Maintb;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
+
+    Artisan::call('migrate:fresh');    
   
-  return   $jsonobj= json_decode (file_get_contents(base_path('test.JSON') ),true);
+     $jsonobj= json_decode (file_get_contents(base_path('test.JSON') ),true);
+
+   
+
+    $data=[
+        'gstin' =>$jsonobj['gstin'],
+        'fp' =>$jsonobj['fp'],
+        'b2b' =>json_encode($jsonobj['b2b']),
+
+    ];
+
+
+
+     $dbs= Maintb::create($data);
+   return  $maindbs = Maintb::all();
+
+
+
+  
 
 });
